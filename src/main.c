95cba0722c6f90873166b515b01ca288fbec67d6
Igor De Nadai <igoroliveiradenadai@gmail.com>
2026-09-06 15:51:35 -0300

    add function type. Add header main.h

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char builtinListe[][10] = {"echo", "exite", "type"};
+// List of shell biuld-in commands
+char builtinList[][10] = {"echo", "exit", "type"};
 
 int main(int argc, char *argv[])
 {
-  char eingabe[100];
+  char input[100];
 
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -15,29 +17,88 @@ int main(int argc, char *argv[])
   {
     printf("$ ");
 
-    fgets(eingabe, 100, stdin);
-
-    // Wait for user input
-    char input[100];
     fgets(input, 100, stdin);
 
-    // Remove the trailing newline
-    input[strlen(input) - 1] = '\0';
+    // memcmp compare two data type byte by byte, and the
+    // number of bytes to compare
 
-    // COMMAND: exit
-    // strcmp: string comparison. if equal, return 0
-    if (strcmp(input, "exit") == 0)
+    if (memcmp(input, "exit", 4) == 0)
     {
-      break;
+      return 0;
     }
-    else if (strncmp(input, "echo ", 5) == 0) // COMMAND: echo
+    else if (memcmp(input, "echo", 4) == 0)
     {
-      printf("%s\n", input + 5);
+      printf("%s", &input[5]);
+    }
+    else if (memcmp(input, "type", 4) == 0)
+    {
+      char *parameter = &input[5];
+      int n = lineLength(parameter);
+      parameter[n] = '\0';
+      printf("%s", parameter);
+
+      int isBuiltin = 0;
+      int isExecutable = 0;
+      int isUnknown = 1;
+
+      // sizeof(builtinList)/sizeof(builtinList[0]) gives the vector length. Used it before, on other project
+      for (int i = 0; i < sizeof(builtinList) / sizeof(builtinList[0]); i++)
+      {
+        if (memcmp(parameter, builtinList[i], stringLength(builtinList[i])) == 0)
+        {
+          isBuiltin = 1;
+          isUnknown = 0;
+        }
+      }
+      if (isBuiltin != 0)
+      {
+        printf(" is a shell builtin \n");
+      }
+      else if (isExecutable != 0)
+      {
+        printf("is an executable \n");
+      }
+      else if (isUnknown != 0)
+      {
+        printf(": not found \n");
+      }
     }
     else
     {
-      printf("%s: command not found\n", input);
+      int n = lineLength(input);
+      input[n] = '\0';
+      printf(input);
+      printf(": command not found\n");
     }
   }
+
   return 0;
+}
+int stringLength(char *string)
+{
+  if (string == NULL)
+  {
+    return -1;
+  }
+  int result = 0;
+
+  while (string[result] != '\0')
+  {
+    ++result;
+  }
+  return result;
+}
+int lineLength(char *string)
+{
+  if (string == NULL)
+  {
+    return -1;
+  }
+  int result = 0;
+
+  while ((string[result] != '\0') && (string[result] != '\n'))
+  {
+    ++result;
+  }
+  return result;
 }
