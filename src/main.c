eb8eff451cd7521f077ea5e0ad9e2990690af580
Igor De Nadai <igoroliveiradenadai@gmail.com>
2026-09-06 14:14:31 -0300

    add echo

--- src/main.c
+++ src/main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+char builtinListe[][10] = {"echo", "exite", "type"};
+
 int main(int argc, char *argv[])
 {
+  char eingabe[100];
+
   // Flush after every printf
   setbuf(stdout, NULL);
 
@@ -11,14 +15,29 @@ int main(int argc, char *argv[])
   {
     printf("$ ");
 
+    fgets(eingabe, 100, stdin);
+
     // Wait for user input
     char input[100];
     fgets(input, 100, stdin);
 
     // Remove the trailing newline
     input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
-  }
 
+    // COMMAND: exit
+    // strcmp: string comparison. if equal, return 0
+    if (strcmp(input, "exit") == 0)
+    {
+      break;
+    }
+    else if (strncmp(input, "echo ", 5) == 0) // COMMAND: echo
+    {
+      printf("%s\n", input + 5);
+    }
+    else
+    {
+      printf("%s: command not found\n", input);
+    }
+  }
   return 0;
 }
